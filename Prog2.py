e288e515f4a4df43ab744f99c430cfcfa5d9e2b5
bharathb04 <38512697+bharathb04@users.noreply.github.com>
2018-04-19 04:13:00 +0000

    Create Prog2.py

---
+++ Prog2.py
@@ -0,0 +1,23 @@
+# Hello World program in Python
+#!/usr/bin/python
+import sys
+##print "Counted", len(data), "lines."
+#learning python 
+print "Hello, Python!"
+######
+
+list_a = [7,8,14,2,3,14,1,18]
+
+list_b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+list_c = []
+
+   
+for element in list_b:
+
+    if element < 5:
+       list_c.append(element)    
+       print(element)
+
+
+print list_c
