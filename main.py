4ed86319dd347321c8f9bfd37a82cde578d55f17
bharathb04 <38512697+bharathb04@users.noreply.github.com>
2018-04-19 00:42:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+# Hello World program in Python
 #!/usr/bin/python
-
+import sys
+##print "Counted", len(data), "lines."
 #learning python 
 print "Hello, Python!"
+name = sys.stdin.readline()
+age = int(sys.stdin.readline())
+
+print "Your name is " + name
+
+print "Your reamining year to reach 100 is " + str((100-age))
+
+
+
